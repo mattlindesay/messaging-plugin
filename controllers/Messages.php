2ba39734941c5f01d95a7eb96e368c51e1c28853
root <root@master>
2016-08-11 20:36:36 +0000

    added nav and controller

---
+++ controllers/Messages.php
@@ -0,0 +1,18 @@
+<?php namespace Lindesay\Messaging\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Messages extends Controller
+{
+    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController'];
+    
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Lindesay.Messaging', 'main-menu-item', 'side-menu-item');
+    }
+}
